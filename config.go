e50cede6dc01e23f85f438f3867ab15d1ae007f7
Yash Maurya <iyashmaurya@gmail.com>
2026-03-03 00:40:38 +0530

    refactor: improve naming consistency, centralize error handling, and shift API validation to client layer

--- config.go
+++ config.go
@@ -7,70 +7,62 @@ import (
 
 type Config struct {
 	APIKey             string
-	APIUrl             string
+	APIURL             string
 	BatchSize          int
 	IdealBatchItemSize int
 }
 
+type ConfigOption func(c *Config) error
 
-type ConfigOption func (c *Config) error
-
-
-func SetUrl (url string) ConfigOption {
-	return func (c *Config) error {
+func SetURL(url string) ConfigOption {
+	return func(c *Config) error {
 		if !(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
-			return fmt.Errorf("API URL must be a valid HTTP/HTTPS URL")
+			return fmt.Errorf("lingo: api url must be a valid http/https url")
 		}
-		c.APIUrl = url
+		c.APIURL = url
 		return nil
 	}
 }
 
-func SetBatchSize (batch int) ConfigOption {
-	return func (c *Config) error {
-		if (batch < 1 || batch > 250) {
-			return fmt.Errorf("Batch Size should be between 1-250")
+func SetBatchSize(batch int) ConfigOption {
+	return func(c *Config) error {
+		if batch < 1 || batch > 250 {
+			return fmt.Errorf("lingo: batch size should be between 1-250")
 		}
 		c.BatchSize = batch
-		return nil 
-	}
-}
-
-func SetIdealBatchItemSize (idelsize int) ConfigOption {
-	return func(c *Config) error {
-		if (idelsize < 1 || idelsize > 2500) {
-			return fmt.Errorf("Batch Size should be between 1-2500")
-		} 
-		c.IdealBatchItemSize = idelsize
 		return nil
 	}
 }
 
+func SetIdealBatchItemSize(size int) ConfigOption {
+	return func(c *Config) error {
+		if size < 1 || size > 2500 {
+			return fmt.Errorf("lingo: ideal batch item size should be between 1-2500")
+		}
+		c.IdealBatchItemSize = size
+		return nil
+	}
+}
 
-
-func NewEngineConfig (apiKey string, opts ...ConfigOption) (*Config, error) {
+func newEngineConfig(apiKey string, opts ...ConfigOption) (*Config, error) {
 	const (
-		defaultAPIUrl = "https://engine.lingo.dev"
-		defaultBatchSize = 25
+		defaultAPIURL             = "https://engine.lingo.dev"
+		defaultBatchSize          = 25
 		defaultIdealBatchItemSize = 250
 	)
-	
-	if apiKey == "" {
-		return nil, fmt.Errorf("lingo: api key is required")
-	}
-	
+
 	c := &Config{
-		APIKey: apiKey,
-		APIUrl:  defaultAPIUrl,
-		BatchSize: defaultBatchSize,
+		APIKey:             apiKey,
+		APIURL:             defaultAPIURL,
+		BatchSize:          defaultBatchSize,
 		IdealBatchItemSize: defaultIdealBatchItemSize,
 	}
-	
+
 	for _, opt := range opts {
 		if err := opt(c); err != nil {
 			return nil, err
 		}
 	}
-	
+
 	return c, nil
 }
