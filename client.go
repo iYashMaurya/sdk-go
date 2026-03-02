43aff632fb11704dbfb0c5a504a088321da4e4dd
Yash Maurya <iyashmaurya@gmail.com>
2026-03-03 00:45:16 +0530

    feat: introduce Client with configurable initialization and HTTP client

---
+++ client.go
@@ -0,0 +1,29 @@
+package lingo
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	config Config
+	httpClient *http.Client
+}
+
+func NewClient(apiKey string, opts ...ConfigOption) (*Client, error) {
+	if apiKey == "" {
+		return nil, fmt.Errorf("lingo: api key is required")
+	}
+	config, err  := newEngineConfig(apiKey, opts...)
+	if err != nil {
+		return nil, err
+	}
+	c := &Client{
+		config: *config,
+		httpClient: &http.Client{
+			Timeout: 60*time.Second,
+		},
+	}
+	return c, nil
+}
